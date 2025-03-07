0a0d2b2b183322322f55837e41600cb59fa6c128
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-07 22:33:04 +0500

    todat topice is conversion operation in javascript

---
+++ conversionOpration.js
@@ -0,0 +1,13 @@
+// to day we learn about conversion operation in javascript
+// so what is conversion operation?
+// conversion operation is a process of converting one data type into another data type
+// there are 2 types of conversion operation in javascript
+// 1. implicit conversion
+// 2. explicit conversion
+// now let see them in detial
+// 1. implicit conversion
+// implicit conversion is a process of converting one data type into another data type automatically by javascript
+let num = 10;
+let num1 = "10";
+let sum = num + num1;
+console.log(sum);
