813efaa3bafe55252bced939349205c5feedc8e2
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-18 01:32:43 +0500

    numbers and maths in js

---
+++ 06_numbers.js
@@ -0,0 +1,29 @@
+// so today let talk about numbers and math in javascript
+// so first talk about numbers how we can use them in javascript and realy time projects what defficulity we can face and how we can solve them
+// so first talk about what is number in javascript?
+// number is a basically a data type in javascript that is used to store numbers
+// so how we can stroe numbers in javascript?
+// we can store numbers in javascript by using numbers
+// so how we can declare numbers in javascript?
+// we can declare numbers in javascript by using numbers
+// let me show you how we can declare numbers in javascript
+let number = 10;
+
+console.log(number);
+// so this is how we can declare numbers in javascript
+// now its time to talk about some math in javascript and how we can use it in javascript
+// first talk about some basic math in javascript
+// how we can do addition in javascript?
+//  we can do addition in javascript by using + operator
+// let me show you some examples
+let num = 10 + 10;
+console.log(num);
+// this is a simple example of addition
+// let try some complex addition
+let num1 = 500;
+let num2 = 100;
+let add = num1 + num2;
+console.log(add);
+// let see subtraction
+let sub = num1 - num2;
+console.log(sub);
