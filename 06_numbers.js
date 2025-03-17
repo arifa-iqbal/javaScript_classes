71a219b062514dda54377f44811bc58337d7ada3
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-18 02:30:06 +0500

    compeleted number topic in js

--- 06_numbers.js
+++ 06_numbers.js
@@ -8,8 +8,35 @@
 // we can declare numbers in javascript by using numbers
 // let me show you how we can declare numbers in javascript
 let number = 10;
-
 console.log(number);
+// which method we can use in numbers?
+// we can use all the methods in numbers that we can use in strings
+//  let me show you
+console.log(number.toString());
+console.log(number.toFixed(0));
+console.log(number.toExponential());
+console.log(number.toPrecision(2));
+// so these are some methods that we can use in numbers
+// we can also use sime maths methods in numbers
+// let me show
+console.log(Math.abs(-20));
+// this method id used to get the absolute value of a number we can also use this method in strings and other data types
+// in math.abs we can change the sign of a number not all signs but only negitive in to positive
+console.log(Math.ceil(4.3));
+// this method is used to get the ceil value of a number
+console.log(Math.floor(4.3));
+// this method is used to get the floor value of a number now what is the difference between ceil and floor?
+// ceil is used to get the upper value of a number and floor is used to get the lower value of a number
+console.log(Math.round(4.5));
+// math.rendom can give us values between 0 and 1  for example 0.1, 0.2,... etc
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// console.log(number);
 // so this is how we can declare numbers in javascript
 // now its time to talk about some math in javascript and how we can use it in javascript
 // first talk about some basic math in javascript
