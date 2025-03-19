e48bed3a5c5d7ac64cf245be8ba3377206e64395
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-20 01:10:43 +0500

    arrays in js

---
+++ basicJSPart-2/01_Array.js
@@ -0,0 +1,18 @@
+// so today we are going to study about an array in javascript
+// now what is an array?
+// an array is a special type of object used to store multiple values in a single variable. array can hold elements of any data type it include number, string, object and even other array.
+// arrays are dynamic , means their size can be cahnge as elements are added or removed.
+// let do some practice by creating an array
+// how we can careate an empty array
+let arrEmpty = [];
+console.log(arrEmpty);
+// let see with number data type
+let numArry = [1, 2, 3, 4, 5];
+console.log(numArry);
+// in console we can observe that the length shown as 5 but the index show 4 this is because in array its start whith 0 index.
+// let see what if we did this
+let arr = new Array(5);
+console.log(arr);
+// this is arry with specified length but in result it shows the empty arry with length 5.its just for learn perpose
+
+// let further learn about array methods in javascript
