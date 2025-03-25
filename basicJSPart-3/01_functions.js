66b4a496408015ae6c7ee9db9eefdbbc61690292
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-25 07:21:00 +0500

    how to creat a function

---
+++ basicJSPart-3/01_functions.js
@@ -0,0 +1,9 @@
+// today we are going to learn about functions in javascript, how founctions can work? why they are important?
+// lets learn with realy life examples
+// first let see what is function?
+// so functions in javascript is a block of code designed to perform a particular task. for example
+function greet() {
+  console.log("hello world");
+}
+greet();
+// function greet() {console.log("hello world");} this pice of code can not work util we call the function to execute the function it is importent to call the function and we can call function  greet(); like this
