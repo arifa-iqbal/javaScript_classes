249511e4b83d7ee2be8417b0620dccffcab4d25e
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-28 00:20:41 +0500

    more about scope

--- basicJSPart-3/02_scops.js
+++ basicJSPart-3/02_scops.js
@@ -12,12 +12,28 @@ let a = 20;
 // this is function scope
 function myNumber() {
   let a = 60;
-  console.log(a);
+  //   console.log(a);
 }
 myNumber();
 // this is block scope
 {
   let a = 500;
-  console.log(a);
+  //   console.log(a);
 }
-console.log(a);
+// console.log(a);
+// nisted functions and how the scope work in nisted function
+function first() {
+  const user = "arifa";
+  //   the child
+  function second() {
+    const email = "arifa@gmail.com";
+
+    console.log(user); // here we access the value of parent
+  }
+
+  //   console.log(email); //   access the child value. note when we try to access the child value ourside of the second function we find an error here we know that the child can access the value of parent but the parent is unable to access the value of his child.
+
+  second(); //   call the child function
+}
+
+first(); // call parent function
