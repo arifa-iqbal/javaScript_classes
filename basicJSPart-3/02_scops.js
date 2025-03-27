8e93c5cce24cbb71468abcb32721d54e6dcbe88b
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-27 23:25:16 +0500

    understanding with examples

--- basicJSPart-3/02_scops.js
+++ basicJSPart-3/02_scops.js
@@ -6,3 +6,18 @@
 // 1. Global scope: variables declared outside of any function or block are in the global scope.
 // 2. function scope: variables declared inside the function. thwy are accessed within function only
 // 3. block scope: variables declared with let and const inside a block ({}). accessed in the block only
+// let see some examples
+// this is globle scope
+let a = 20;
+// this is function scope
+function myNumber() {
+  let a = 60;
+  console.log(a);
+}
+myNumber();
+// this is block scope
+{
+  let a = 500;
+  console.log(a);
+}
+console.log(a);
