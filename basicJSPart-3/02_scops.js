96629b926bb426e47811a2ba851d657053483532
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-27 23:11:15 +0500

    detail about scop

--- basicJSPart-3/02_scops.js
+++ basicJSPart-3/02_scops.js
@@ -1,1 +1,4 @@
 // today  we are going to learn about scop in javascript
+// in simple word the {} are scop
+// Scope in JavaScript:
+// in java script scope refers to the accessibility or visibility of variables, functions and objects in different parts of our code. it determines where variables can be accessed or modified.
