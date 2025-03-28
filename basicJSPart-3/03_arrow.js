00ead23ab3e506f69284a0a555e1d0308af46338
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-03-29 00:37:22 +0500

    using this in function

--- basicJSPart-3/03_arrow.js
+++ basicJSPart-3/03_arrow.js
@@ -1,1 +1,21 @@
 // arrow functions and this concept
+// what is "this" in function
+// "this" refers the current context for example
+const user = {
+  name: "arifa ",
+  email: "arifa@gamil.com",
+  welcommessage: function message() {
+    console.log(`${this.name}, welcome `); // for using current context we can use "this"
+
+    // console.log(this);  //  what if we use only "this" it returns all the context in side the variable
+  },
+};
+
+user.welcommessage(); // here we find the same value
+// but what if i change the value of the name? what is the ans let see
+user.name = "iqbal";
+// let call the function again
+user.welcommessage();
+// and the ans is the current value means this returns the current value "iqbal"
+// what if use "this" out here
+console.log(this);
