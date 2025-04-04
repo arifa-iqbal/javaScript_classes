2aa6a5124ecabb074a436fb25766f483fada1d3a
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-05 00:42:29 +0500

    if condition

--- controlFlow-4/1.js
+++ controlFlow-4/1.js
@@ -2,4 +2,15 @@
 // and how to use it in javascript
 // so the control flow is the order in which the code is executed and its is important to understande how it works in javascript.
 // how we can use it in our code
-//
+// the code should run on condition if all the code run at the same time it will be a mess
+// let see what are the condition we can use in javascript
+// the first one is if statement
+// the if statement is used to execute a block of code if the condetion is true
+//  the syntex of if statement is as follow
+// if (condition) {the code to be executed if the condition is true}
+// let see an example
+// let say we have a variable called age and we want to check if the age is greater than 18 then we will print a message to the console
+let age = 20;
+if (age > 18) {
+  console.log("you are an adult");
+}
