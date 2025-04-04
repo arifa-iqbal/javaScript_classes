6df98140ede07d05baafb342ebfd13706af9e5f3
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-04 23:59:22 +0500

    about work flow

---
+++ controlFlow-4/1.js
@@ -0,0 +1,3 @@
+// to we talk about the control flow in javascript
+// and how to use it in javascript
+// so the control flow is the order in which the code is executed and its is important to understande how it works in javascript.
