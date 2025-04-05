e14ae2c1ae4527163f29909e10db6de5070e1947
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-06 02:35:54 +0500

    example

--- controlFlow-4/1.js
+++ controlFlow-4/1.js
@@ -34,6 +34,13 @@ if (age > 18) {
 // the syntex of else if statement is as follow
 // if (condotion) {the code to be executed if the condition is true } else if (condition){ the code to be executed if the condition is true } else {the code to be executed if the condition is false}
 // let see an example
-// let say we have a variable called age and we want to check if the age is greater than 18 then we will print a message to the console if the age is less than 18 then we will print a message to the console if the age is equal to 18 then we will print a message to the console 
+// let say we have a variable called age and we want to check if the age is greater than 18 then we will print a message to the console if the age is less than 18 then we will print a message to the console if the age is equal to 18 then we will print a message to the console
 let age1 = 18;
-if 
+if (age1 > 18) {
+  console.log("you are eligible to vote");
+}
+if (age1 < 18) {
+  console.log("you are not eligible to vote");
+} else {
+  console.log("you are 18 years old");
+}
