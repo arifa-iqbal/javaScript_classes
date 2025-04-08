e076aa09b63f16a5f10123613fe980d9cf94a2c0
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-08 20:57:33 +0500

    defination and types of loop

---
+++ loops-5/loops1.js
@@ -0,0 +1,6 @@
+// today we will start loops. loops are also called iteration
+// loops are used to repeat a block of code multiple times
+// there are 3 types of loops in javascript
+// 1. for loop
+// 2. while loop
+// 3. do while loop
