b3c6be672def80f18bb669d68e224eb6b7c679e0
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-09 17:03:34 +0500

    example

---
+++ loops-5/whileloop.js
@@ -0,0 +1,17 @@
+// lets talk about while loop in javascript
+// while loop is used to execute a block of code as long as the condition is true
+// while loop is used when we dont know how many times the loop will run
+// while loop syntax as follows
+//while(condition){code to be executed}
+
+// example of ewhile loop
+// let i = 0;
+// while (i < 5) {
+//   console.log("i is less than 5", i);
+//   i++;
+// }
+let i = 0;
+while (i <= 20) {
+  console.log(`the value of i is ${i}`);
+  i++;
+}
