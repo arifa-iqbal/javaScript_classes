dabf042eea92f810d5f8737b7c0f8c0c210dba4b
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-10 13:35:33 +0500

    use of for of loop over string

--- loops-5/arrayloops.js
+++ loops-5/arrayloops.js
@@ -2,6 +2,11 @@
 // first is for of loop
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (const number of array) {
-  console.log(number);
+  //   console.log(number);
 }
-//
+// we can also use this loop over strings
+// let see how it works on strings
+const greeting = "hello world!";
+for (const greet of greeting) {
+  console.log(`each char is ${greet}`);
+}
