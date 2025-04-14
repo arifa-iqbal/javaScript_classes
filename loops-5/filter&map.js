499bfa2efe7ad977878ee5d0dc8655013fad006c
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-14 12:16:34 +0500

    same example with foreach loop

--- loops-5/filter&map.js
+++ loops-5/filter&map.js
@@ -2,6 +2,15 @@
 // filter is a method that creates a new array with all elements that pass the test implemented by the provided function
 // let understand filter with example
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const newNbrs = numbers.filter((num) => num > 5);
-console.log(newNbrs);
+// const newNbrs = numbers.filter((num) => num > 5);
+
 // so filter returns a callback function that returns a new array with all elements that pass the test implemented by the provided function
+// what if i want to do the same work in foreach loop
+
+const newNumbers = [];
+numbers.forEach((num) => {
+  if (num > 5) {
+    newNumbers.push(num);
+  }
+});
+console.log(newNumbers);
