5a0068dcbe08492b1c790b5ebb5dc7d9b8d4eadc
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-16 16:17:17 +0500

    reduce method

--- loops-5/reduce.js
+++ loops-5/reduce.js
@@ -5,9 +5,25 @@
 //  the accumulator is the value returned by the percious call of the callback function
 //  the current value is the current element of the array
 //  let see an example of reduce method
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const initialValue = 0;
-const sum = numbers.reduce((accumulation, currentValue) => {
-  return accumulation + currentValue;
-}, initialValue);
-console.log(sum);
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const sum = numbers.reduce((accumulation, currentValue) => {
+//   return accumulation + currentValue;
+// }, 0);
+// console.log(sum);
+// lets create a card where we can see the sum of item which we buy
+const items = [
+  { books: 2, price: 80 },
+  {
+    vegtables: "2kg",
+    price: 100,
+  },
+  { fruits: "1kg", price: 50 },
+  { milk: "2l", price: 120 },
+  { eggs: "1dozen", price: 60 },
+];
+const totalPrice = items.reduce((accumulation, currentValue) => {
+  return accumulation + currentValue.price;
+}, 0);
+console.log(totalPrice);
+// so our total price is 410
