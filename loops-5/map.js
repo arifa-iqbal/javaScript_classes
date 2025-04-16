566a368cb51af2137294267f836d3e26c7e16df0
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-16 14:29:32 +0500

    intro and example of map

---
+++ loops-5/map.js
@@ -0,0 +1,11 @@
+// let see what is map in javascript
+// map is a higher order function that takes a function as an agrument and applies it to each element in the array and returns a new array with the results
+// map is a non mutating function that does not change the orginal array
+// now what is non nutating function?
+// non mutating function is a function that does not change the orginal array or object
+// let understand this with an example
+const numbers = [1, 2, 3, 4, 5, 6];
+const newNums = numbers.map((num) => {
+  return num + 10; // if we use scops in any function , we should use return ststement to return the value of the function other wise it will return undefined
+});
+console.log(newNums);
