67be62c8a38d81372936e9eee40a60efd62080b7
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-16 14:39:41 +0500

    chaning in javascript

--- loops-5/map.js
+++ loops-5/map.js
@@ -5,7 +5,13 @@
 // non mutating function is a function that does not change the orginal array or object
 // let understand this with an example
 const numbers = [1, 2, 3, 4, 5, 6];
-const newNums = numbers.map((num) => {
-  return num + 10; // if we use scops in any function , we should use return ststement to return the value of the function other wise it will return undefined
-});
+// const newNums = numbers.map((num) => {
+//   return num + 10; // if we use scops in any function , we should use return ststement to return the value of the function other wise it will return undefined
+// });
+
+// let see how chaning work in javascript
+const newNums = numbers
+  .map((num) => num * 10)
+  .map((num) => num + 5)
+  .filter((num) => num >= 20);
 console.log(newNums);
