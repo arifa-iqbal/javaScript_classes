fd1ec6165a68354b61ee5d215377a975d3d1bc4f
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-25 21:39:24 +0500

    useing evenlistner

--- projects-7/pro1.js
+++ projects-7/pro1.js
@@ -1,2 +1,13 @@
 // let create the project
-document.querySelectorAll(".button");
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e);
+    console.log(e.target);
+    // let use if else to change the color of the
+    if (e.target.style.backgroundColor === "black") {
+      body.style.backgroundImage = e.target.style.backgroundColor;
+    }
+  });
+});
