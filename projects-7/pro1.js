deb2028441e0de8d8b9e329b301c4a667e735c53
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-26 13:41:37 +0500

    same projects with different methods

--- projects-7/pro1.js
+++ projects-7/pro1.js
@@ -1,13 +1,30 @@
 // let create the project
+// const buttons = document.querySelectorAll(".button");
+// const body = document.querySelector("body");
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (e) {
+//     const color = e.target.id;
+//     body.style.backgroundColor = color;
+//   });
+// });
+// let do the same thing with if statement
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log(e);
-    console.log(e.target);
-    // let use if else to change the color of the
-    if (e.target.style.backgroundColor === "black") {
-      body.style.backgroundImage = e.target.style.backgroundColor;
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    const color = e.target.id;
+    if (color === "blue") {
+      body.style.backgroundColor = "blue";
+    } else if (color === "gray") {
+      body.style.backgroundColor = "gray";
+    } else if (color === "white") {
+      body.style.backgroundColor = "white";
+    } else if (color === "black") {
+      body.style.backgroundColor = "black";
+    } else if (color === "yellow") {
+      body.style.backgroundColor = "yellow";
+    } else {
+      body.style.backgroundColor = "white";
     }
   });
 });
