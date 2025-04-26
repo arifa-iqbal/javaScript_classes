1f7a54a3064609323e6c30b8db9abcbf9ca95068
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-26 14:48:15 +0500

    if statement

--- projects-7/pro1.js
+++ projects-7/pro1.js
@@ -29,29 +29,53 @@
 //   });
 // });
 // let do the same with switch statement
+// const buttons = document.querySelectorAll(".button");
+// const body = document.querySelector("body");
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (e) {
+//     const color = e.target.id;
+//     switch (color) {
+//       case "blue":
+//         body.style.backgroundColor = "blue";
+//         break;
+//       case "gray":
+//         body.style.backgroundColor = "gray";
+//         break;
+//       case "white":
+//         body.style.backgroundColor = "white";
+//         break;
+//       case "black":
+//         body.style.backgroundColor = "black";
+//         break;
+//       case "yellow":
+//         body.style.backgroundColor = "yellow";
+//         break;
+//       default:
+//         body.style.backgroundColor = "white";
+//     }
+//   });
+// });
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
-    const color = e.target.id;
-    switch (color) {
-      case "blue":
-        body.style.backgroundColor = "blue";
-        break;
-      case "gray":
-        body.style.backgroundColor = "gray";
-        break;
-      case "white":
-        body.style.backgroundColor = "white";
-        break;
-      case "black":
-        body.style.backgroundColor = "black";
-        break;
-      case "yellow":
-        body.style.backgroundColor = "yellow";
-        break;
-      default:
-        body.style.backgroundColor = "white";
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = "blue";
+    }
+    if (e.target.id === "gray") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "black") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
     }
   });
 });
