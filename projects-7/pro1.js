6c0079e4e7e71d7f5fd323840cab4ce9a247c326
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-26 16:41:32 +0500

    we use different methods

--- projects-7/pro1.js
+++ projects-7/pro1.js
@@ -77,5 +77,14 @@ buttons.forEach(function (button) {
     if (e.target.id === "purple") {
       body.style.backgroundColor = e.target.id;
     }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "orange") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "pink") {
+      body.style.backgroundColor = e.target.id;
+    }
   });
 });
