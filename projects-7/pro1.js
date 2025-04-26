4b96fb6caac3e2e40f11cb171131f13fefa76768
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-26 13:50:51 +0500

    same project with switch method

--- projects-7/pro1.js
+++ projects-7/pro1.js
@@ -8,23 +8,50 @@
 //   });
 // });
 // let do the same thing with if statement
+// const buttons = document.querySelectorAll(".button");
+// const body = document.querySelector("body");
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (e) {
+//     const color = e.target.id;
+//     if (color === "blue") {
+//       body.style.backgroundColor = "blue";
+//     } else if (color === "gray") {
+//       body.style.backgroundColor = "gray";
+//     } else if (color === "white") {
+//       body.style.backgroundColor = "white";
+//     } else if (color === "black") {
+//       body.style.backgroundColor = "black";
+//     } else if (color === "yellow") {
+//       body.style.backgroundColor = "yellow";
+//     } else {
+//       body.style.backgroundColor = "white";
+//     }
+//   });
+// });
+// let do the same with switch statement
 const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
     const color = e.target.id;
-    if (color === "blue") {
-      body.style.backgroundColor = "blue";
-    } else if (color === "gray") {
-      body.style.backgroundColor = "gray";
-    } else if (color === "white") {
-      body.style.backgroundColor = "white";
-    } else if (color === "black") {
-      body.style.backgroundColor = "black";
-    } else if (color === "yellow") {
-      body.style.backgroundColor = "yellow";
-    } else {
-      body.style.backgroundColor = "white";
+    switch (color) {
+      case "blue":
+        body.style.backgroundColor = "blue";
+        break;
+      case "gray":
+        body.style.backgroundColor = "gray";
+        break;
+      case "white":
+        body.style.backgroundColor = "white";
+        break;
+      case "black":
+        body.style.backgroundColor = "black";
+        break;
+      case "yellow":
+        body.style.backgroundColor = "yellow";
+        break;
+      default:
+        body.style.backgroundColor = "white";
     }
   });
 });
