de180d7d4bb1ac6de3ba3e4697d80fe952cd3841
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-27 19:40:50 +0500

    ......

---
+++ projects-7/project4/app.js
@@ -0,0 +1,5 @@
+const sumitBtn = document.querySelector("#submt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector("#guesses");
+const remaining = document.querySelector("#lastResult");
+const lowOrHi = document.querySelector("#lowOrHi");
