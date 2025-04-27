3200d791cfcce94e66ac525b80ebb9f09281c849
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-27 15:11:31 +0500

    2 project is completed

--- projects-7/project2/app.js
+++ projects-7/project2/app.js
@@ -1,19 +1,20 @@
 // let create BMI calculator
 const form = document.querySelector("form");
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
   const height = parseInt(document.querySelector("#height").value);
-  const weigth = parseInt(document.querySelector("#weigth").value);
+  const weight = parseInt(document.querySelector("#weight").value);
   const result = document.querySelector("#result");
   // here we will use if else statement to check the BMI
 
   if (height === "" || height < 0 || isNaN(height)) {
     result.innerHTML = `please enter a valid height ${height}`;
-  } else if (weigth === "" || weigth < 0 || isNaN(weigth)) {
-    result.innerHTML = `please enter a valid weigth ${weigth}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    result.innerHTML = `please enter a valid weight ${weight}`;
   } else {
     // formula to calculte Bmi
-    const bmi = (weigth / ((height * height) / 10000)).toFixed(2);
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
     // show result
     result.innerHTML = `<span>${bmi}</span>`;
     // i want to print an other message based on the bmi value that the value comes in underweight, nomal, or overweight
