2e3edacc7b67b5a2ec7484d223b0e7543ac394ac
arifa-iqbal <arifaiqbal1996@gmail.com>
2025-04-28 15:32:16 +0500

    creating a number guess game

--- projects-7/project4/app.js
+++ projects-7/project4/app.js
@@ -3,3 +3,38 @@ const userInput = document.querySelector("#guessField");
 const guessSlot = document.querySelector("#guesses");
 const remaining = document.querySelector("#lastResult");
 const lowOrHi = document.querySelector("#lowOrHi");
+
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const p = document.createElement("p");
+
+let preGuess = [];
+let numberGuess = 1;
+let palyGame = true;
+if (palyGame) {
+  sumitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    validateGuess(guess);
+  });
+}
+
+// function form validation
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("please enter a number between 1 and 100");
+  } else if (guess < 1) {
+    alert("please enter a number greater than 1");
+  } else if (guess > 100) {
+    alert("please enter a number less than 100");
+  }
+}
+// function to check the guess
+function checkGuess(guess) {}
+// function to display the guess
+function displayGuess(guess) {}
+// function to display message
+function displayMessage(message) {}
+// function for the end of the game
+function endGame() {}
+// function to new game
+function newGame() {}
